d1086b2ce0efb47c2961b7f05dd9b29472729fc1
davidpro08 <davidmoon0817@gmail.com>
2024-06-24 18:22:32 +0900

    백엔드에서 로그인 기능

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -7,18 +7,22 @@ const loginBtn = document.querySelector("button");
 
 loginBtn.addEventListener("click", login)
 
-function login(){
+function login() {
     const req = {
-        id : id.value,
-        psword : psword.value,
+        id: id.value,
+        psword: psword.value,
     };
 
-    
-    fetch("/login",{
-        method:"POST",
-        headers :{
-            "Content-Type":"application/json",
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
         },
-        body:JSON.stringify(req),
+        body: JSON.stringify(req),
+    })
+    .then((res) =>res.json())
+    .then((res)=>{
+        
     });
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,21 +1,43 @@
 "use strict"
 
 const output = {
-    hello :(req, res)=>{
+    hello: (req, res) => {
         res.render("home/index");
     },
-    
-    login : (req, res)=>{
+
+    login: (req, res) => {
         res.render("home/login");
     },
-}
+};
+
+const users = {
+    id: ["davidpro", "배성빈", "유성우"],
+    psword: ["moon0817", "tyzm쇼크", "ryu0101"],
+};
 
 const process = {
-    login : (req, res)=>{
-        console.log(req.body);
+    login: (req, res) => {
+        const id = req.body.id;
+        const psword = req.body.psword;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+                return res.json({
+                    success:true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인이 실패했습니다.",
+        });
     }
 }
-module.exports={
+
+
+module.exports = {
     output,
     process,
 };
