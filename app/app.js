b85970b52955dd09a079868b049ef56e9dab890d
davidpro08 <davidmoon0817@gmail.com>
2024-06-24 16:52:24 +0900

    로그인 API 만들기

--- app/app.js
+++ app/app.js
@@ -1,21 +1,8 @@
-// const http = require("http");
-// const app = http.createServer((req,res)=>{
-//     res.writeHead(200, {"Content-Type" : "text/html; charset=utf-8"});
-//     if(req.url==='/'){
-//         res.end("여기는 루트입니다");
-//     }else if(req.url==="/login"){
-//         res.end("여기는 로그인 화면입니다.")
-//     }
-// });
-
-// app.listen(3001, ()=>{
-//     console.log("http로 가동된 서버입니다.");
-// });
-
 "use strict";
 
 //모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 //라우팅
@@ -24,8 +11,9 @@ const home = require("./src/routes/home");
 //앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
-
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드
 
