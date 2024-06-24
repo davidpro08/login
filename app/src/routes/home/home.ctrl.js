132ec932bb05e8155cafbe2d544815cac7107de1
davidpro08 <davidmoon0817@gmail.com>
2024-06-24 22:31:44 +0900

    객체지향 맛보기

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,8 @@
 "use strict"
 
+const UserStorage = require("../../models/UserStorage");
+
+
 const output = {
     hello: (req, res) => {
         res.render("home/index");
@@ -20,19 +23,20 @@ const process = {
         const id = req.body.id;
         const psword = req.body.psword;
 
+        const users = UserStorage.getUsers("id", "psword");
+
+        const response = {};
         if (users.id.includes(id)) {
             const idx = users.id.indexOf(id);
             if (users.psword[idx] === psword) {
-                return res.json({
-                    success:true,
-                });
+                response.success = true;
+                return res.json(response);
             }
         }
 
-        return res.json({
-            success: false,
-            msg: "로그인이 실패했습니다.",
-        });
+        response.success = false;
+        response.msg = "로그인이 실패했습니다."
+        return res.json(response);
     }
 }
 
