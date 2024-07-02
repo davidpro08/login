a248875cac3528ae9ef9058093b76f03fae65059
davidpro08 <davidmoon0817@gmail.com>
2024-07-02 15:37:32 +0900

    User 객체 만들기

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -4,7 +4,7 @@ class UserStorage{
     static #users = {
         id: ["davidpro", "charliepro", "ryu0101"],
         psword: ["moon0817", "bae1206", "ryu0101"],
-        name: ["문재현", "배성빈", "유성우"],
+        name: ["문재현", "배성빈", "유성우", "문한나"],
     };
 
     static getUsers(...fields){
@@ -17,6 +17,18 @@ class UserStorage{
         }, {});
         return newUsers;
     };
+
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users); // =>{id, psword, name}
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+
+        return userInfo;
+    }
 }
 
 module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict"
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User.js");
 
 
 const output = {
@@ -13,29 +13,11 @@ const output = {
     },
 };
 
-const users = {
-    id: ["davidpro", "배성빈", "유성우"],
-    psword: ["moon0817", "tyzm쇼크", "ryu0101"],
-};
-
 const process = {
     login: (req, res) => {
-        const id = req.body.id;
-        const psword = req.body.psword;
-
-        const users = UserStorage.getUsers("id", "psword");
-
-        const response = {};
-        if (users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if (users.psword[idx] === psword) {
-                response.success = true;
-                return res.json(response);
-            }
-        }
-
-        response.success = false;
-        response.msg = "로그인이 실패했습니다."
+        const user = new User(req.body);
+        const response = user.login();
+        console.log(response);
         return res.json(response);
     }
 }
