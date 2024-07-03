d0e88de379302fa1e529ae9439662b6a04901be9
davidpro08 <davidmoon0817@gmail.com>
2024-07-03 15:14:51 +0900

    db로 로그인 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,12 +1,24 @@
 "use strict";
 
-const fs = require("fs");
+const fs = require("fs").promises;
 
-class UserStorage{
-    static getUsers(...fields){
+class UserStorage {
+
+    static #getUserInfo(data, id) {
+        const users = JSON.parse(data);
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users); // =>{id, psword, name}
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+        return userInfo;
+    }
+
+    static getUsers(...fields) {
         //const users = this.#users;
-        const newUsers = fields.reduce((newUsers, field)=>{
-            if(users.hasOwnProperty(field)){
+        const newUsers = fields.reduce((newUsers, field) => {
+            if (users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
             }
             return newUsers;
@@ -15,27 +27,21 @@ class UserStorage{
     };
 
     static getUserInfo(id) {
-        fs.readFile("./src/data/users.json" ,(err, data)=>{
-            if(err) throw err;
-            const users = JSON.parse(data);
-            const idx = users.id.indexOf(id);
-            const usersKeys = Object.keys(users); // =>{id, psword, name}
-            const userInfo = usersKeys.reduce((newUser, info) => {
-                newUser[info] = users[info][idx];
-                return newUser;
-            }, {});
-    
-            return userInfo;
-        });
+        return fs.readFile("./src/data/users.json")
+            .then((data) => {
+                return this.#getUserInfo(data, id);
+            })
+            .catch(console.error);
     }
 
-    static save(userInfo){
+    static save(userInfo) {
         //const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
-        return {success : true}
+        return { success: true }
     }
+
 }
 
 module.exports = UserStorage;
